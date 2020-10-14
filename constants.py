db8e32fe711cebcabad45c83f9228183167a3b4f
andre <andres.benoit7@gmail.com>
2020-10-14 16:33:12 -0300

    Adding a flag for motor case and bulkhead analysis

--- constants.py
+++ constants.py
@@ -13,36 +13,9 @@ alpha_case = float(k_case / (cp_case * rho_case))                               
 # -----------------------------------------------
 # Convection Coefficient
 h_m = 1295                                                                                  # Average convection coefficient [W/m^2-K]
+
 #-------------------------------------------------
 # Temperature
 Ta = 297                                                                                    # Ambient temperature in Tatuí-SP-Brazil in the month of August [K]
 Tc = 1600                                                                                   # Chamber temperature [K]
-# ------------------------------------------------
-# Inputs
-# Radial coordinate
-insulator_thk = 0.003                                                                       # Insulation thickness [m]
-case_thk = 0.00555                                                                          # Case thickness |VERIFY THIS VALUEEEE| [m]
-r_1 = 0.11                                                                                  # Radial position of insulation beginning [m]
-r_2 = r_1 + insulator_thk                                                                   # Radial position of insulation/casing interface [m]
-r_3 = r_2 + case_thk                                                                        # Radial position of casing end [m]
-sectionsr = 500                                                                             # Nº of sections between r_1 and r_3
-dr = (r_3 - r_1) / sectionsr                                                                # r variation [m]
-nr = sectionsr + 1                                                                          # Nº of radial points
 
-# Height coordinate
-z = 1.42                                                                                    # Height of combustion chamber [m]
-dz = dr                                                                                     # z variation [m]
-sectionsz = math.ceil(z / dz)                                                               # Nº of sections beewtween 0 and z
-nz = sectionsz + 1                                                                          # Nº of z points
-
-# Time
-t = 5                                                                                      # Burn time [s]
-nt = 500
-dt = t / nt                                                                                 # Time variation [s]
-
-# Stability constants
-Fo_insulator = alpha_insulator * (dt / (dr ** 2))                                           # Insulation Fourier number
-Fo_case = alpha_case * (dt / (dr ** 2))                                                     # Casing Fourier number
-
-Bi_insulator = h_m * (dr) / k_insulator                                                     # Insulation Biot number
-Bi_case = h_m * (dr) / k_case                                                               # Casing Biot number
--- main.py
+++ main.py
@@ -1,41 +1,67 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from constants import *
 
-# we have to set the initial/boundary contitions
-r = [r_1 + i * dr for i in range(nr)]
-z = [i * dz for i in range(nz)]
+# Asking to the user what kind of analisys it will be running
+print('1 - Motor case\n2 - Bulkhead\n')
+choose = int(input('Choose: '))
 
-#Creating the A matrix
-A = np.zeros([nr, nr])
+if choose==1:
+    # Setting the radial and coordinates, also setting the burn time and steps
+    # ------------------------------------------------
+    # Inputs
+    # Radial coordinate
+    insulator_thk = 0.003                                                                       # Insulation thickness [m]
+    case_thk = 0.00555                                                                          # Case thickness |VERIFY THIS VALUEEEE| [m]
+    r_1 = 0.11                                                                                  # Radial position of insulation beginning [m]
+    r_2 = r_1 + insulator_thk                                                                   # Radial position of insulation/casing interface [m]
+    r_3 = r_2 + case_thk                                                                        # Radial position of casing end [m]
+    sectionsr = int(input('Radial and z steps: '))                                              # Nº of sections between r_1 and r_3
+    dr = (r_3 - r_1) / sectionsr                                                                # r variation [m]
+    nr = sectionsr + 1                                                                          # Nº of radial points
 
-#Building the A matrix
-A[0][0] = 1 + (2*dt*k_insulator)/(rho_insulator*cp_insulator*dr**2)
-A[0][1] = -k_insulator*2*dt / (dr*dr*rho_insulator*cp_insulator)
-A[nr-1][nr-1] = 1 - alpha_case * dt / (r[nr-1] * dr) - alpha_case * dt / (dr ** 2)
-A[nr-1][nr-2] = alpha_case * dt / (r[nr-1] * dr) + 2 * alpha_case * dt / (dr ** 2)
-A[nr-1][nr-3] = -alpha_case * dt / (dr ** 2)
+    # Height coordinate
+    z = float(input('Motor lenght [m]: '))                                                      # Height of combustion chamber [m]
+    dz = dr                                                                                     # z variation [m]
+    sectionsz = math.ceil(z / dz)                                                               # Nº of sections beewtween 0 and z
+    nz = sectionsz + 1                                                                          # Nº of z points
 
-for i in range(1, nr-1):
-   if r[i] >= r_2: alpha = alpha_case
-   else: alpha = alpha_insulator
-   A[i][i - 1] = -(alpha * dt) / (dr ** 2) + alpha * dt / (2 * dr * r[i])
-   A[i][i] = 1 + 2 * alpha * dt / (dr ** 2)
-   A[i][i + 1] = -(alpha * dt) / dr ** 2 - alpha * dt / (2 * dr * r[i])
+    # Time
+    t = float(input('Burn time [s]: '))                                                         # Burn time [s]
+    nt = int(input('Time steps: '))                                                             # Time steps
+    dt = t / nt                                                                                 # Time variation [s]
 
-#Creating the T matrix
-T = np.zeros([nt, nr])
+    # we have to set the initial and boundary contitions
+    r = [r_1 + i * dr for i in range(nr)]
+    z = [i * dz for i in range(nz)]
 
-#Setting initial values for the T matrix
-for i in range(nr):
-     T[0][i] = Ta
+    #Creating the A matrix
+    A = np.zeros([nr, nr])
 
-#Getting the inverse matrix of A
-A_inverse = np.linalg.inv(A)
+    #Building the A matrix
+    A[0][0] = 1 + (2*dt*k_insulator)/(rho_insulator*cp_insulator*dr**2)
+    A[0][1] = -k_insulator*2*dt / (dr*dr*rho_insulator*cp_insulator)
+    A[nr-1][nr-1] = 1 - alpha_case * dt / (r[nr-1] * dr) - alpha_case * dt / (dr ** 2)
+    A[nr-1][nr-2] = alpha_case * dt / (r[nr-1] * dr) + 2 * alpha_case * dt / (dr ** 2)
+    A[nr-1][nr-3] = -alpha_case * dt / (dr ** 2)
 
-# Calculating the T matrix
-for i in range(nt-1):
-  T[i][0] = T[i][0] + 2*dt*h_m*(Tc-T[i][0])/(rho_insulator*cp_insulator*dr)
-  T[i+1] = (np.matmul(A_inverse, T[i]))
+    for i in range(1, nr-1):
+       if r[i] >= r_2: alpha = alpha_case
+       else: alpha = alpha_insulator
+       A[i][i - 1] = -(alpha * dt) / (dr ** 2) + alpha * dt / (2 * dr * r[i])
+       A[i][i] = 1 + 2 * alpha * dt / (dr ** 2)
+       A[i][i + 1] = -(alpha * dt) / dr ** 2 - alpha * dt / (2 * dr * r[i])
 
-#isajdaisehasioehasuheaeasd
+    #Creating the T matrix
+    T = np.zeros([nt, nr])
+
+    #Setting initial values for the T matrix
+    for i in range(nr):
+         T[0][i] = Ta
+
+    #Getting the inverse matrix of A
+    A_inverse = np.linalg.inv(A)
+
+    # Calculating the T matrix
+    for i in range(nt-1):
+      T[i][0] = T[i][0] + 2*dt*h_m*(Tc-T[i][0])/(rho_insulator*cp_insulator*dr)
+      T[i+1] = (np.matmul(A_inverse, T[i]))
