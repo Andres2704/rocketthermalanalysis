1765dce688396636d93d049cb08972623f562d92
Andres Benoit <andres.benoit7@gmail.com>
2020-10-14 22:32:54 -0300

    Create a moto_case() class

--- main.py
+++ main.py
@@ -1,67 +1,98 @@
 import numpy as np
-from constants import *
+from materials import *
+from gui import *
 
-# Asking to the user what kind of analisys it will be running
-print('1 - Motor case\n2 - Bulkhead\n')
-choose = int(input('Choose: '))
+class motor_case():
+    def __init__(self, material_case, material_liner,  insulator_thk, case_thk, r_steps, hm, Tc, Ta, motor_lenght,  burn_time, t_steps):
+        self.rho_case, self.k_case, self.cp_case = case_selector(material_case)
+        self.rho_insulator, self.k_insulator, self.cp_insulator = insulator_selector(material_liner)
+        self.insulator_thk = insulator_thk
+        self.case_thk = case_thk
+        self.sectionsr = r_steps
+        self.z = motor_lenght
+        self.h_m = hm
+        self.Tc = Tc
+        self.Ta = Ta
+        self.t = burn_time
+        self.nt = t_steps
+        self.alpha_case =  float(self.k_case / (self.cp_case * self.rho_case))
+        self.alpha_insulator = float(self.k_insulator / (self.cp_insulator * self.rho_insulator))
+        self.T = self.run_analysis()
 
-if choose==1:
-    # Setting the radial and coordinates, also setting the burn time and steps
-    # ------------------------------------------------
-    # Inputs
-    # Radial coordinate
-    insulator_thk = 0.003                                                                       # Insulation thickness [m]
-    case_thk = 0.00555                                                                          # Case thickness |VERIFY THIS VALUEEEE| [m]
-    r_1 = 0.11                                                                                  # Radial position of insulation beginning [m]
-    r_2 = r_1 + insulator_thk                                                                   # Radial position of insulation/casing interface [m]
-    r_3 = r_2 + case_thk                                                                        # Radial position of casing end [m]
-    sectionsr = int(input('Radial and z steps: '))                                              # Nº of sections between r_1 and r_3
-    dr = (r_3 - r_1) / sectionsr                                                                # r variation [m]
-    nr = sectionsr + 1                                                                          # Nº of radial points
+    def run_analysis(self):
+        # Radial coordinate
+        self.r_1 = 0.11  # Radial position of insulation beginning [m]
+        self.r_2 = self.r_1 + self.insulator_thk  # Radial position of insulation/casing interface [m]
+        self.r_3 = self.r_2 + self.case_thk  # Radial position of casing end [m]
+        self.dr = (self.r_3 - self.r_1) / self.sectionsr  # r variation [m]
+        self.nr = self.sectionsr + 1  # Nº of radial points
 
-    # Height coordinate
-    z = float(input('Motor lenght [m]: '))                                                      # Height of combustion chamber [m]
-    dz = dr                                                                                     # z variation [m]
-    sectionsz = math.ceil(z / dz)                                                               # Nº of sections beewtween 0 and z
-    nz = sectionsz + 1                                                                          # Nº of z points
+        # Height coordinate
+        self.dz = self.dr  # z variation [m]
+        self.sectionsz = int(np.ceil(self.z / self.dz))  # Nº of sections beewtween 0 and z
+        print(self.sectionsz)
+        self.nz = self.sectionsz + 1  # Nº of z points
 
-    # Time
-    t = float(input('Burn time [s]: '))                                                         # Burn time [s]
-    nt = int(input('Time steps: '))                                                             # Time steps
-    dt = t / nt                                                                                 # Time variation [s]
+        # Time
+        self.dt = self.t / self.nt  # Time variation [s]
 
-    # we have to set the initial and boundary contitions
-    r = [r_1 + i * dr for i in range(nr)]
-    z = [i * dz for i in range(nz)]
+        # we have to set the initial and boundary contitions
+        self.r = [self.r_1 + i * self.dr for i in range(self.nr)]
+        self.z = [i * self.dz for i in range(self.nz)]
 
-    #Creating the A matrix
-    A = np.zeros([nr, nr])
+        A = self.create_Amatrix()
 
-    #Building the A matrix
-    A[0][0] = 1 + (2*dt*k_insulator)/(rho_insulator*cp_insulator*dr**2)
-    A[0][1] = -k_insulator*2*dt / (dr*dr*rho_insulator*cp_insulator)
-    A[nr-1][nr-1] = 1 - alpha_case * dt / (r[nr-1] * dr) - alpha_case * dt / (dr ** 2)
-    A[nr-1][nr-2] = alpha_case * dt / (r[nr-1] * dr) + 2 * alpha_case * dt / (dr ** 2)
-    A[nr-1][nr-3] = -alpha_case * dt / (dr ** 2)
+        # Creating the T matrix
+        T = np.zeros([self.nt, self.nr])
 
-    for i in range(1, nr-1):
-       if r[i] >= r_2: alpha = alpha_case
-       else: alpha = alpha_insulator
-       A[i][i - 1] = -(alpha * dt) / (dr ** 2) + alpha * dt / (2 * dr * r[i])
-       A[i][i] = 1 + 2 * alpha * dt / (dr ** 2)
-       A[i][i + 1] = -(alpha * dt) / dr ** 2 - alpha * dt / (2 * dr * r[i])
+        # Setting initial values for the T matrix
+        for i in range(self.nr):
+            T[0][i] = self.Ta
 
-    #Creating the T matrix
-    T = np.zeros([nt, nr])
+        # Getting the inverse matrix of A
+        A_inverse = np.linalg.inv(A)
 
-    #Setting initial values for the T matrix
-    for i in range(nr):
-         T[0][i] = Ta
+        # Calculating the T matrix
+        for i in range(self.nt - 1):
+            T[i][0] = T[i][0] + 2 * self.dt * self.h_m * (self.Tc - T[i][0]) / (self.rho_insulator * self.cp_insulator * self.dr)
+            T[i + 1] = (np.matmul(A_inverse, T[i]))
 
-    #Getting the inverse matrix of A
-    A_inverse = np.linalg.inv(A)
+        return T
 
-    # Calculating the T matrix
-    for i in range(nt-1):
-      T[i][0] = T[i][0] + 2*dt*h_m*(Tc-T[i][0])/(rho_insulator*cp_insulator*dr)
-      T[i+1] = (np.matmul(A_inverse, T[i]))
+    def create_Amatrix(self):
+        # Creating the A matrix
+        A = np.zeros([self.nr, self.nr])
+
+        # Building the A matrix
+        A[0][0] = 1 + (2 * self.dt * self.k_insulator) / (self.rho_insulator * self.cp_insulator * self.dr ** 2)
+        A[0][1] = -self.k_insulator * 2 * self.dt / (self.dr * self.dr * self.rho_insulator * self.cp_insulator)
+        A[self.nr - 1][self.nr - 1] = 1 - self.alpha_case * self.dt / (self.r[self.nr - 1] * self.dr) - self.alpha_case * self.dt / (self.dr ** 2)
+        A[self.nr - 1][self.nr - 2] = self.alpha_case * self.dt / (self.r[self.nr - 1] * self.dr) + 2 * self.alpha_case * self.dt / (self.dr ** 2)
+        A[self.nr - 1][self.nr - 3] = -self.alpha_case * self.dt / (self.dr ** 2)
+
+        for i in range(1, self.nr - 1):
+            if self.r[i] >= self.r_2:
+                alpha = self.alpha_case
+            else:
+                alpha = self.alpha_insulator
+            A[i][i - 1] = -(alpha * self.dt) / (self.dr ** 2) + alpha * self.dt / (2 * self.dr * self.r[i])
+            A[i][i] = 1 + 2 * alpha * self.dt / (self.dr ** 2)
+            A[i][i + 1] = -(alpha * self.dt) / self.dr ** 2 - alpha * self.dt / (2 * self.dr * self.r[i])
+        return A
+
+
+if __name__ == "__main__":
+    import sys
+    material_case = 'aluminium6061t6'
+    material_liner = 'epdm'
+    insulatior_thk = 0.003
+    case_thk = 0.00555
+    r_steps = 500
+    hm = 1295
+    Tc = 1600
+    Ta = 297
+    lenght = 1.42
+    burn_time = 5
+    t_steps = 500
+    motor = motor_case(material_case, material_liner, insulatior_thk, case_thk, r_steps, hm, Tc, Ta, lenght, burn_time, t_steps)
+    print(motor.T)
