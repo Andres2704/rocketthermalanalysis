7981682aaea21e5363777ded51ca33008e558109
joaovrohr <72899046+joaovrohr@users.noreply.github.com>
2020-10-17 01:18:12 -0300

    Update explicit.py

--- explicit.py
+++ explicit.py
@@ -11,7 +11,6 @@ k_case = 167                                      #Casing thermal conductivity [
 rho_case = 2700                                   #Casing density [kg/m3]
 cp_case = 896                                     #Casing specific heat capacity [J/kg-K]
 alpha_case = float(k_case / (cp_case * rho_case)) #Casing thermal diffusivity [m^2/s]
-
 #-----------------------------------------------
 #Convection Coefficient
 h_m = 1295  #Average convection coefficient [W/m^2-K]
@@ -19,31 +18,31 @@ h_m = 1295  #Average convection coefficient [W/m^2-K]
 #------------------------------------------------
 #Inputs
 #Radial coordinate
-insulator_thk = 0.003         #Insulation thickness [m]
-case_thk = 0.00538            #Case thickness |VERIFY THIS VALUEEEE| [m]
-r_1 = 0.11                    #Radial position of insulation beginning [m]
-r_2 = r_1 + insulator_thk     #Radial position of insulation/casing interface [m]
-r_3 = r_2 + case_thk          #Radial position of casing end [m]
-sectionsr = 100                #Nº of sections between r_1 and r_3
-dr = (r_3 - r_1) / sectionsr  #r variation [m]
-nr = sectionsr + 1            #Nº of radial points
+radius_outer = 0.07           # Chamber outer radius [m]
+radius_inner = 0.064          # Chamber inner radius [m]
+sectionsr = 100              #Nº of sections between r_1 and r_3
+nr = sectionsr + 1
+dr = (radius_outer)/ sectionsr  #r variation [m]
+r_1 = dr                 #Radial position of insulation beginning [m]
+r_2 = r_1 + radius_inner      #Radial position of insulation/casing interface [m]
+r_3 = radius_outer          #Radial position of casing end [m]
+
 
 #Height coordinate
-z = 1.42         #Height of combustion chamber [m]
-sectionsz = 25   #Nº of sections beewtween 0 and z
-dz = z/sectionsz #z variation [m]
-nz = sectionsz+1 #Nº of z points
+insulator_thk = 0.003         #Insulation thickness [m]
+z = 0.007        #Thickness of bulkhead [m]
+z_1 = 0
+z_2 = insulator_thk
+z_3 = z_2 + z
+dz = dr/1.4 #z variation [m]
+nz = math.ceil(z/dz) #Nº of z points
 
 #Time
-t = 7                                                                        #Burn time [s]
-dt_insulator = (k_insulator*dr**2)/(4*alpha_insulator*(k_insulator+h_m*dr))  #Stability dt for insulator [s]
-dt_case = (k_case*dr**2)/(4*alpha_case*(k_case+h_m*dr))                      #Stability dt for casing [s]
-if dt_insulator >= dt_case: dt = dt_case
-else: dt = dt_insulator                                                      #t variation [s]
+t = 7                                                                  #Burn time [s]
+
+dt = ((dr**2)*(dz**2))/(1.99*alpha_case*(dr**2+dz**2))
 
 nt = math.ceil(t / dt)                                                       #Nº of time points
-
-print(nt)
 
 d_insulator = alpha_insulator * (dt / (dr**2))  #Insulation Fourier number
 d_case = alpha_case * (dt / (dr**2))            #Casing Fourier number
@@ -54,6 +53,28 @@ Tc = 1600  #Chamber temperature [K] -> This is a given value, yet this is not th
 
 #we have to set the initial/boundary contitions
 r = [r_1 + i * dr for i in range(nr)]
+z = [z_1 + i * dr for i in range(nz)]
+"""""
+print(dr)
+print(dz)
+print(nz)
+
+print("Insulator")
+print("Middle z and start r:")
+print((dz**2)/(2*alpha_case))
+print("Middle z and middle r:")
+print(1/((2*alpha_case/(dr**2)+2*alpha_case/(dz**2))))
+print(((dr**2)*(dz**2))/(2*alpha_case*(dr**2+dz**2)))
+print("Middle z and end r:")
+print((dz**2)*(dr**2)/(2*alpha_case*(dr**2-dz**2)))
+print("Start z and start r")
+print((rho_case*cp_case)/(k_case/dr**2+k_case/dz**2+2*h_m/dz))
+print("Start z and middle r")
+print((rho_case*cp_case*dz**2)/(k_case+2*h_m*dz))
+print("End z and Middle r")
+print((dr**2)*(dz**2)/(alpha_case*(2*dz**2-dr**2)))
+"""""
+
 
 T = np.zeros([nt, nr, nz])
 for i in range(nr):
@@ -61,42 +82,39 @@ for i in range(nr):
     T[0][i][j] = Ta
 
 #Calculating the T-matrix
-for j in range(nt - 1):
-    for i in range(nr):
-      for l in range(nz):
-        if r[i] <= r_2:  #Set insulation material
+for i in range(nt - 1):
+    for l in range(nz):
+      for j in range(nr):
+        if z[l] <= z_2:  #Set insulation material
             alpha = alpha_insulator
             rho = rho_insulator
             cp = cp_insulator
             k = k_insulator
-        if r[i] > r_2:   #Set casing material
+        if z[l] > z_2:   #Set casing material
             alpha = alpha_case
             rho = rho_case
             cp = cp_case
             k = k_case
-        if i == nr - 1:
-          if l == nz - 1:   #r end and z end
-            T[j + 1][i][l] = T[j][i][l] + alpha * dt * (((T[j][i][l]-2*T[j][i][l-1]+T[j][i][l-2])/(dz**2))+((T[j][i][l] - T[j][i - 1][l])/(r[i] * dr)) + ((T[j][i][l] + T[j][i-2][l] - 2 * T[j][i-1][l]) / (dr**2)))
-          elif l == 0:      #r end and z start
-            T[j + 1][i][l] = T[j][i][l] + alpha * dt * (((T[j][i][l]-2*T[j][i][l+1]+T[j][i][l+2])/(dz**2))+((T[j][i][l] - T[j][i - 1][l])/(r[i] * dr)) + ((T[j][i][l] + T[j][i-2][l] - 2 * T[j][i-1][l]) / (dr**2)))
-          else:             #r end and z middle
-            T[j + 1][i][l] = T[j][i][l] + alpha * dt * (((T[j][i][l+1]-2*T[j][i][l]+T[j][i][l-1])/(dz**2))+((T[j][i][l] - T[j][i - 1][l])/(r[i] * dr)) + ((T[j][i][l] + T[j][i-2][l] - 2 * T[j][i-1][l]) / (dr**2)))
-        elif i == 0:
-          if l == nz-1:     #r start and z end
-            T[j + 1][i][l] = T[j][i][l] + ((2 * dt) / (rho * cp * dr * dz)) * (h_m * dz * (Tc - T[j][i][l]) + k * dz * (T[j][i+1][l] - T[j][i][l])/dr + k * dr * (T[j][i][l] -T[j][i][l-1])/(2 * dz))
-          elif l == 0:      #r start and z start
-            T[j + 1][i][l] = T[j][i][l] + ((2 * dt) / (rho * cp * dr * dz)) * (h_m * dz * (Tc - T[j][i][l]) + k * dz * (T[j][i+1][l] - T[j][i][l])/dr + k * dr * (T[j][i][l+1] - T[j][i][l])/(2 * dz))
-          else:             #r start and z middle
-            T[j + 1][i][l] = T[j][i][l] + ((2 * dt) / (rho * cp * dr * dz)) * (h_m * dz * (Tc - T[j][i][l]) + k * dz * (T[j][i+1][l] - T[j][i][l])/dr + k * dr * (T[j][i][l+1] - T[j][i][l-1])/(2 * dz))
+        if l == nz-1:
+            if j == nr-1:   #z end and r end
+                T[i + 1][j][l] = (1 + (alpha * dt) / (r[j] * dr) + (alpha * dt) / (dr ** 2) + (alpha * dt) / (dz ** 2)) * T[i][j][l] + ((-2*alpha*dt)/(dr**2) - (alpha*dt)/(r[j]*dr))*T[i][j-1][l] + ((alpha*dt)/(dr**2))*T[i][j-2][l] + ((-2*alpha*dt)/(dz**2))*T[i][j][l-1] + ((alpha*dt)/(dz**2))*T[i][j][l-2]
+            elif j == 0:    #z end and r start
+                T[i+1][j][l] = (1 - (alpha*dt)/(r[j]*dr) + (alpha*dt)/(dr**2) + (alpha*dt)/(dz**2))*T[i][j][l] + ((alpha*dt)/(r[j]*dr) - (2*alpha*dt)/(dr**2))*T[i][j+1][l] + ((alpha*dt)/(dr**2))*T[i][j+2][l] + ((-2*alpha*dt)/(dz**2))*T[i][j][l-1] + ((alpha*dt)/(dz**2))*T[i][j][l-2]
+            else:           #z end and r middle
+                T[i + 1][j][l] = (1 - (2*alpha*dt)/(dr**2) + (alpha*dt)/(dz**2))*T[i][j][l] + ((alpha*dt)/(dr**2) - (alpha*dt)/(2*r[j]*dr))*T[i][j-1][l] + ((alpha*dt)/(dr**2) + (alpha*dt)/(2*r[j]*dr))*T[i][j+1][l] + ((-2*alpha*dt)/(dz**2))*T[i][j][l-1] + ((alpha*dt)/(dz**2))*T[i][j][l-2]
+        elif l == 0:
+            if j == nr-1:   #z start and r end
+                T[i + 1][j][l] = (2 * h_m * dt * Tc) / (rho * cp * dz) + (1 + (k*dt)/(rho*cp*(dr)**2) - (k*dt)/(rho*cp*(dz)**2) - (2*h_m*dt)/(rho*cp*dz))*T[i][j][l] + ((-k*dt)/(rho*cp*(dr)**2))*T[i][j-1][l] + ((k*dt)/(rho*cp*(dz)**2))*T[i][j][l+1]
+            elif j == 0:    #z start and r start
+                T[i + 1][j][l] = (2*h_m*dt*Tc)/(rho*cp*dz) + (1 - (k*dt)/(rho*cp*(dr)**2) - (k*dt)/(rho*cp*(dz)**2) - (2*h_m*dt)/(rho*cp*dz))*T[i][j][l] + ((k*dt)/(rho*cp*(dr)**2))*T[i][j+1][l] + ((k*dt)/(rho*cp*(dz)**2))*T[i][j][l+1]
+            else:           #z start and r middle
+                T[i + 1][j][l] = (2 * h_m * dt * Tc) / (rho * cp * dz) + (1 - (k*dt)/(rho*cp*(dz)**2) - (2*h_m*dt)/(rho*cp*dz))*T[i][j][l] + ((k*dt)/(rho*cp*(dr)**2))*T[i][j+1][l] + ((-k*dt)/(rho*cp*(dr)**2))*T[i][j-1][l] + ((k*dt)/(rho*cp*(dz)**2))*T[i][j][l+1]
         else:
-          if l == nz-1:     #r middle and z end
-            T[j + 1][i][l] = T[j][i][l] + alpha * dt * ((
-                (T[j][i+1][l] - T[j][i-1][l])/(2 * r[i] * dr)) + ((T[j][i+1][l] + T[j][i-1][l-2] - 2 * T[j][i][l]) / (dr**2)) + ((T[j][i][l] + T[j][i][l-2] - 2*T[j][i][l-1]) / (dz**2)))
-          elif l == 0:      #r middle and z start
-            T[j + 1][i][l] = T[j][i][l] + alpha * dt * ((
-                (T[j][i+1][l] - T[j][i-1][l])/(2 * r[i] * dr)) + ((T[j][i+1][l] + T[j][i-1][l-2] - 2 * T[j][i][l]) / (dr**2)) + ((T[j][i][l] + T[j][i][l+2] - 2*T[j][i][l+1]) / (dz**2)))
-          else:             #r middle and z middle
-            T[j + 1][i][l] = T[j][i][l] + alpha * dt * ((
-                (T[j][i+1][l] - T[j][i-1][l])/(2 * r[i] * dr)) + ((T[j][i+1][l] + T[j][i-1][l-2] - 2 * T[j][i][l]) / (dr**2)) + ((T[j][i][l+1] + T[j][i][l-1] - 2*T[j][i][l]) / (dz**2)))
+            if j == nr-1:   #z middle and r end
+                T[i+1][j][l] = (1+(alpha*dt)/(r[j]*dr) + (alpha*dt)/(dr**2) - (2*alpha*dt)/(dz**2))*T[i][j][l] + ((-2*alpha*dt)/(dr**2) - (alpha*dt)/(r[j]*dr))*T[i][j-1][l] + ((alpha*dt)/(dr**2))*T[i][j-2][l] + ((alpha*dt)/(dz**2))*T[i][j][l-1] + ((alpha*dt)/(dz**2))*T[i][j][l+1]
 
+            elif j == 0:    #z middle and r start
+                T[i+1][j][l] = (1- (alpha*dt)/(r[j]*dr) + (alpha*dt)/(dr**2)  - (2*alpha*dt)/(dz**2))*T[i][j][l] + ((alpha*dt)/(r[j]*dr) - (2*alpha*dt)/(dr**2))*T[i][j+1][l] + ((alpha*dt)/(dr**2))*T[i][j+2][l] + ((alpha*dt)/(dz**2))*T[i][j][l-1] + ((alpha*dt)/(dz**2))*T[i][j][l+1]
+            else:           #z middle and r middle
+                T[i + 1][j][l] = (1 - (2*alpha*dt)/(dr**2) - (2*alpha*dt)/(dz**2))*T[i][j][l] + ((alpha*dt)/(dr**2) - (alpha*dt)/(2*r[j]*dr))*T[i][j-1][l] + ((alpha*dt)/(dr**2) + (alpha*dt)/(2*r[j]*dr))*T[i][j+1][l] + ((alpha*dt)/(dz**2))*T[i][j][l-1] + ((alpha*dt)/(dz**2))*T[i][j][l+1]
 print(T)
